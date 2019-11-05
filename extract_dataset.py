0d13076502e998c65d332ab6aa91f5725f8afe38
Niklas <niklas@lindorfer.com>
2019-11-05 11:07:25 +0000

    Added FLIR data extraction script

---
+++ extract_dataset.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+import argparse
+import numpy as np
+import flirimageextractor
+from skimage.io import imsave
+
+
+def extract_image(flir, path):
+    flir.process_image(path)
+    thermal = flir.get_thermal_np()
+    rgb = flir.extract_embedded_image()
+    return thermal, rgb
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Extract thermal and visible data from FLIR One exported images.")
+    parser.add_argument("in_path", help="Input directory containing exported images")
+    parser.add_argument("out_path", help="Output directory for extracted dataset")
+    args = vars(parser.parse_args())
+
+    # Setup output dir  
+    shutil.rmtree(args["out_path"], ignore_errors=False, onerror=None)
+    os.makedirs(os.path.join(args["out_path"], "thermal"))
+    os.makedirs(os.path.join(args["out_path"], "visible"))
+
+    flir = flirimageextractor.FlirImageExtractor(palettes=[])
+
+    images = filter(lambda f: f.endswith(".jpg"), os.listdir(args["in_path"]))
+    for image in images:
+        thermal, visible = extract_image(flir, os.path.join(args["in_path"], image))
+        imsave(os.path.join(args["out_path"], "thermal", image), thermal)
+        imsave(os.path.join(args["out_path"], "visible", image), visible)

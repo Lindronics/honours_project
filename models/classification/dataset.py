de56aaee7747584d4185eb90f2bab972ef626a55
Niklas <niklas@lindorfer.com>
2020-02-26 13:37:36 +0000

    Added ability to load all data into memory to Dataset class * Also added documentation

--- models/classification/dataset.py
+++ models/classification/dataset.py
@@ -13,8 +13,29 @@ transformation = np.array([
 ])
 
 class Dataset(K.utils.Sequence):
+    """
+    Dataset loader for Keras model.
+    """
 
-    def __init__(self, metadata, rgb_only=False, res=(480, 640), register=True, batch_size=16, split=False, train=True):
+    def __init__(self, metadata:str, rgb_only:bool=False, res:tuple=(480, 640), register:bool=True, batch_size:int=16, split:bool=False, train:bool=True):
+        """
+        Params
+        ------
+        metadata: str
+            Path to file containing annotations
+        rgb_only: bool
+            Whether to load RGB images only and skip LWIR images
+        res: tuple
+            Target resolution of images
+        register: bool
+            Whether to transform RGB images to align them with LWIR images
+        batch_size: int
+            Batch size
+        split: bool
+            Whether to split the data into train and test
+        train: bool
+            Whether to use the train or test portion of the data if split is set
+        """
 
         self.rgb_only = rgb_only
         self.res = res
@@ -43,15 +64,27 @@ class Dataset(K.utils.Sequence):
         self.class_dict = {key: val for val, key in self.classes.items()}
         self.class_labels = [val for key, val in sorted(self.class_dict.items())]
 
-    def __len__(self):
+
+    def __len__(self) -> int:
+        """ Returns the number of batches """
         return len(self.samples) // self.batch_size
-    
-
-    def __iter__(self):
-        return self
 
 
-    def load(self, path, register=False):
+    def load(self, path:str, register:bool=False) -> np.ndarray:
+        """
+        Loads an image at a given file path.
+
+        Params
+        ------
+        path: str
+            Path of the image
+        register: bool
+            Whether to apply affine transformation to image
+
+        Returns
+        -------
+        The loaded image as a ndarray
+        """
         img = tf.io.read_file(path)
         img = tf.image.decode_jpeg(img, channels=3)
         img = tf.image.convert_image_dtype(img, tf.float32).numpy()
@@ -63,7 +96,19 @@ class Dataset(K.utils.Sequence):
         return img
 
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx:int) -> tuple:
+        """
+        Loads a batch of data.
+
+        Params
+        ------
+        idx: int
+            Index of the batch
+        
+        Returns
+        -------
+        Tuple containing the batch image tensor and the one-hot labels
+        """
         if idx < self.__len__():
             X = []
             y = []
@@ -88,11 +133,34 @@ class Dataset(K.utils.Sequence):
         else:
             raise StopIteration
 
-    def get_labels(self):
+
+    def get_all(self) -> tuple:
+        """
+        Loads the whole dataset.
+
+        Returns
+        ------
+        A tensor (n_samples, height, width, channels) and a tensor (n_samples, n_classes)
+        """
+        X, y = [], []
+        for i in range(len(self)):
+            X_batch, y_batch, _ = self.__getitem__(i)
+            X.append(X_batch)
+            y.append(y_batch)
+
+        return np.concatenate(X, 0), np.concatenate(y, 0)
+
+
+    def get_labels(self) -> np.ndarray:
+        """ Gets a list of all labels """
         return np.array([sample[-1] for sample in self.samples])
 
-    def num_classes(self):
+
+    def num_classes(self) -> int:
+        """ Gets the number of classes """
         return len(self.classes)
 
-    def shape(self):
+
+    def shape(self) -> tuple:
+        """ Gets the tensor shape of a sample """
         return self.res[::-1] + tuple([(3 if self.rgb_only else 4)])

c6f06012e8bd6960325edfb7d10cd7aa1261d3db
Niklas <niklas@lindorfer.com>
2020-03-24 18:45:32 +0000

    Dissertation: Inserted grid search result figures

--- models/evaluation/gridsearch_evaluation.py
+++ models/evaluation/gridsearch_evaluation.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-OUT_PATH = "/Users/lindronics/workspace/4th_year/out/out"
+OUT_PATH = "/Users/lindronics/workspace/4th_year/out/new_out"
 
 hists = {}
 for path, _, files in os.walk(OUT_PATH):
@@ -20,28 +20,36 @@ for path, _, files in os.walk(OUT_PATH):
             hists[model] = hists.get(model, {})
             hists[model][config] = hist
 
-
+font = {
+    "fontname": "fbb",
+    # "fontweight": "bold",
+}
 for model in hists:
-    fig, ((ax_loss, ax_acc), (ax_val_loss, ax_val_acc)) = plt.subplots(2, 2)
+    fig, ((ax_loss, ax_acc), (ax_val_loss, ax_val_acc)) = plt.subplots(2, 2, dpi=150)
 
     for config, hist in hists[model].items():
+        if config == "grayscale":
+            continue
+
         ax_loss.plot(hist["loss"], label=config)
         ax_acc.plot(hist["accuracy"], label=config)
         ax_val_loss.plot(hist["val_loss"], label=config)
         ax_val_acc.plot(hist["val_accuracy"], label=config)
 
-    ax_loss.set_title("Training loss")
-    ax_loss.legend()
+    ax_loss.set_title("Training loss", fontdict=font)
+    # ax_loss.legend()
 
-    ax_acc.set_title("Training accuracy")
-    ax_acc.legend()
+    ax_acc.set_title("Training accuracy", fontdict=font)
+    # ax_acc.legend()
 
-    ax_val_loss.set_title("Validation loss")
-    ax_val_loss.legend()
+    ax_val_loss.set_title("Validation loss", fontdict=font)
+    # ax_val_loss.legend()
 
-    ax_val_acc.set_title("Validation accuracy")
-    ax_val_acc.legend()
+    ax_val_acc.set_title("Validation accuracy", fontdict=font)
+    ax_val_acc.legend(prop={"size": 8})
 
-    fig.suptitle(model)
+    # fig.suptitle(model)
+    fig.tight_layout()
+
 
     plt.show()
